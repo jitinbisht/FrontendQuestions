d317263a8c613691230c30f2600d16dcc0231b12
Jitin Bisht <jitinbisht.bisht@gmail.com>
2021-01-26 23:56:25 +0530

    Update DecodeString.js

--- DecodeString.js
+++ DecodeString.js
@@ -1,3 +1,4 @@
+#Type 1:
 function isNumber(char){
   return isNaN(char) === false;
 }
@@ -17,3 +18,30 @@ function decodeString(str) {
 return decodedStr;
 }
 console.log(decodeString("4A3B2C1D2A"));
+
+#Type 2:
+
+
+function decodeString(str) {
+  if(str.length === 0) return '';
+  let decodedStr = '';
+  let currentCount = 0;
+  let currentChar = str[0];
+  str.split('').splice(1).forEach(char => {
+    if(isNumber(char)) {
+      const num = Number(char);
+      currentCount = currentCount * 10 + num;
+    } else {
+    if(currentChar !== char){
+        decodedStr += currentChar.repeat(currentCount);
+        currentCount = 0;
+        currentChar = char;
+    }
+        
+    }
+    });
+return decodedStr += currentChar.repeat(currentCount);
+}
+
+
+console.log(decodeString("A4B3C2D1A2"));
