cce7b994e8df5e4c71bb3367ffa3762695e784f3
Jitin Bisht <jitinbisht.bisht@gmail.com>
2021-02-09 02:49:01 +0530

    Create LongestSubString_No3ConsecutiveChars are Same.js

---
+++ LongestSubString_No3ConsecutiveChars are Same.js
@@ -0,0 +1,20 @@
+ str = “baaabbabbb”
+ 
+ function maxLenSubStr(str){
+      if(str.length < 3) return str.length;
+      
+      let temp = 2;
+      let ans = 2;
+      for(let i=0; i<str.length; i++){
+       if (str.charAt(i) != str.charAt(i - 1) || 
+            str.charAt(i) != str.charAt(i - 2)) 
+            temp++; 
+       else {
+        ans=Math.max(temp, ans);
+        temp=2;
+      }
+       ans=Math.max(temp, ans);
+       return ans;
+ }
+ 
+ maxLenSubStr(str);
