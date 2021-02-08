f87b63e21616f5f6247f4ea365d87adb36695b00
Jitin Bisht <jitinbisht.bisht@gmail.com>
2021-02-09 01:49:10 +0530

    Create InvertBinaryTree.js

---
+++ BinaryTree/InvertBinaryTree.js
@@ -0,0 +1,27 @@
+function InvertBinaryTree(){
+  const queue = [tree];
+  while(queue.length){
+    const current = queue.shift();
+    if(current === null) continue;
+    swapLeftAndRight(current);
+    queue.push(current.left);
+    queue.push(current.right);
+  }
+}
+
+
+function swapLeftAndRight(tree){
+  const left = tree.left;
+  tree.left = tree.right;
+  tree.right = left;
+ }
+  
+  
+ class BinaryTree {
+    constructor(value){
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
