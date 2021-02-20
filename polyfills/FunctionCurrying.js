044669218e678524b1f1d4ce52e8cb1c4d98c33a
Jitin Bisht <jitinbisht.bisht@gmail.com>
2021-02-21 00:36:26 +0530

    Update FunctionCurrying.js

--- polyfills/FunctionCurrying.js
+++ polyfills/FunctionCurrying.js
@@ -1,5 +1,6 @@
 //Reference: https://medium.com/better-programming/currying-inside-javascript-a19f29600880#:~:text=Currying%20is%20an%20advanced%20technique,(s)%20to%20the%20function.
 // Currying is the process of taking in a function that accepts multiple parameters as input and returning a function that accepts a single argument.
+//Currying is a process in functional programming in which we can transform a function with multiple arguments into a sequence of nesting functions. It returns a new function that expects the next argument inline.
 function curry(fn){
   return function curried(...args){
     const done = args.length >= fn.length;
