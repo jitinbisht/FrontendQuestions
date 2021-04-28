b0520cf0f4595d65b326ad658665bdbdc27c5012
Jitin Bisht <bishtjitin@gmail.com>
2021-04-28 23:17:47 +0530

    Create App.js

---
+++ task/checkboxesTask/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+//import ParentComponent from './dummy';
+//import App2 from './test1';
+import ParentComponent from './test2';
+function App() {
+  return (
+    // <ParentComponent/>
+    <ParentComponent/>
+  );
+}
+
+export default App;
