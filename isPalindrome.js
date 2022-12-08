903a7782af139ac280a89e6e8e221ba3088c1615
Jitin Bisht <bishtjitin@gmail.com>
2022-12-08 10:01:16 +0530

    Update isPalindrome.js

--- isPalindrome.js
+++ isPalindrome.js
@@ -8,3 +8,13 @@ const isPalin = (str) =>
   .reduce((result, character) => character + result) === str.trim();
 
 console.log(isPalin(str));
+
+==========
+  
+  function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
