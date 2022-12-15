57736489942cbd96a4091444a222cac29c041114
Jitin Bisht <bishtjitin@gmail.com>
2022-12-15 18:47:11 +0530

    Update FindPathImplement.js

--- FindPathImplement.js
+++ FindPathImplement.js
@@ -16,9 +16,23 @@ var obj = {
     }
 };
 
+
 const findPath = (object, path) => {
-    
-};
+    path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    path = path.replace(/^\./, '');           // strip a leading dot
+    var a = path.split('.');
+    for (var i = 0, n = a.length; i < n; ++i) {
+        var k = a[i];
+        if (k in object) {
+            object = object[k];
+        } else {
+            return;
+        }
+    }
+    return object;
+}
+
+console.log(findPath(obj, 'a.b.c')); // 12
 
 
 console.log(findPath(obj, 'a.b.c')); // 12
