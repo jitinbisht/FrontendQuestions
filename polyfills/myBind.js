b48267b55522b0c5f4bb407bf758a43c2d63a382
Jitin Bisht <bishtjitin@gmail.com>
2023-01-11 11:02:59 +0530

    Update myBind.js

--- polyfills/myBind.js
+++ polyfills/myBind.js
@@ -1,8 +1,19 @@
-Function.prototype.myBind = function (...args) {
-  // throw 'Not implemented!';
-  const obj = this;
-  const params = args.slice(1)
-  return function(...args2){
-    obj.apply(args[0], [...params, ...args2])
-  }
+let obj = {
+  name: 'Jack',
 };
+
+let myFunc = function (id, city) {
+  console.log(`${this.name}, ${id}, ${city}`);  // id will be undefined
+};
+
+// Accepting any number of arguments passed to myBind
+Function.prototype.myBind = function (obj, ...args) {
+  let func = this;
+  // Accepting arguments passed to newFunc
+  return function (...newArgs) {
+    func.apply(obj, [...args, ...newArgs]);
+  };
+};
+
+let newFunc = myFunc.myBind(obj, 'a_random_id')
+newFunc('New York') // Jack, a_random_id, New York
