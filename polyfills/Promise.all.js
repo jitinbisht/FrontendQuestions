b2e7ffc7f9b1328d6708ffe1364f4437b307fb0e
Jitin Bisht <bishtjitin@gmail.com>
2023-01-11 10:40:32 +0530

    Update Promise.all.js

--- polyfills/Promise.all.js
+++ polyfills/Promise.all.js
@@ -1,18 +1,31 @@
-const promiseAll = (promises) => {
-  let result = [];
-  let completedPromises = 0;
+const p1 = Promise.resolve(3);
+const p2 = 1337;
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 100);
+});
+
+
+Promise.myAll = function(promises) {
   return new Promise((resolve, reject) => {
-    promises.forEach((promise, index) => {
-      promise.then((value) => {
-        result[index] = value;
+  	let results = [];
+  	let completedPromises = 0;
+  	promises.forEach((promise, index) =>{
+    	Promise.resolve(promise).then(value => {
+      	results[index] = value;
         completedPromises += 1;
-        if(completedPromises === promises.length) {
-          resolve(results);
+        if(completedPromises === promises.length){
+        	resolve(results)
         }
       })
       .catch(error => {
-        reject(error);
+      	reject(error);
       })
     })
   })
 }
+
+Promise.myAll([p1, p2, p3]).then((values) => {
+  console.log(values); // [3, 1337, "foo"]
+});
